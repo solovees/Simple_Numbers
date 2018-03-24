be4e4bf8b4711a400bf00529997a1c6c00d5d66d
solovees <solovev_e.s@mail.ru>
2018-03-24 12:09:26 +0500

    Добавлена проверка на A B больше нуля. Что A < B

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <strstream>
 #include <lab3_prost.h>
 
 using namespace std;
@@ -13,8 +14,49 @@ void showData(int a, int b){
 
 int main(int argc, char * argv[])
 {
-    int a = atoi(argv[1]);
-    int b = atoi(argv[2]);
-    showData(a,b);
+ /*   int a = atoi(argv[1]);            // функция atoi строку преобразует в число
+      int b = atoi(argv[2]);
+      showData(a,b);  */
+
+
+    if(argc != 3) {
+        std::cerr <<"Количество параметров командной строки меньше допустимого";
+        return 2;
+    }
+
+    std::istrstream astr(argv[1]); // создание первого потока
+    int a;
+    astr >> a;
+    if(astr.fail()) {
+        std::cerr << "Недопустимое значение A";
+        return 3;
+    }
+
+    std::istrstream bstr(argv[2]); // создание второго потока
+    int b;
+    bstr>>b;
+
+    if(astr.fail()) {
+        std::cerr << "Недопустимое значение B";
+        return 4;
+    }
+
+
+    if(a>=b){
+        std::cerr <<"Число A больше B ";
+    }
+
+
+    if(a<=0){
+        std::cerr <<"Число A отрицаетльное или ноль";
+    }
+
+
+    if(b<=0){
+        std::cerr <<"Число B отрицательное или ноль";
+    }
+
+
+
     return 0;
 }
