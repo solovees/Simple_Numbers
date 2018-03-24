61fa76543531b84bb5c6bca35691cca261bc7902
solovees <solovev_e.s@mail.ru>
2018-03-24 12:21:17 +0500

    Добавлен блок try catch

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <strstream>
 #include <lab3_prost.h>
+#include <stdexcept>
 
 using namespace std;
 
@@ -14,49 +15,54 @@ void showData(int a, int b){
 
 int main(int argc, char * argv[])
 {
- /*   int a = atoi(argv[1]);            // функция atoi строку преобразует в число
-      int b = atoi(argv[2]);
-      showData(a,b);  */
+
+    try{
+
+        if(argc != 3) {
+            std::cerr <<"Количество параметров командной строки меньше допустимого";
+            return 2;
+        }
+
+        std::istrstream astr(argv[1]); // создание первого потока
+        int a;
+        astr >> a;
+        if(astr.fail()) {
+            std::cerr << "Недопустимое значение A";
+            return 3;
+        }
+
+        std::istrstream bstr(argv[2]); // создание второго потока
+        int b;
+        bstr>>b;
+
+        if(astr.fail()) {
+            std::cerr << "Недопустимое значение B";
+            return 4;
+        }
 
 
-    if(argc != 3) {
-        std::cerr <<"Количество параметров командной строки меньше допустимого";
-        return 2;
+        if(a>=b){
+            std::cerr <<"Число A больше B ";
+        }
+
+
+        if(a<=0){
+            std::cerr <<"Число A отрицаетльное или ноль";
+        }
+
+
+        if(b<=0){
+            std::cerr <<"Число B отрицательное или ноль";
+        }
+
+
+
+
+        return 0;
     }
 
-    std::istrstream astr(argv[1]); // создание первого потока
-    int a;
-    astr >> a;
-    if(astr.fail()) {
-        std::cerr << "Недопустимое значение A";
-        return 3;
-    }
-
-    std::istrstream bstr(argv[2]); // создание второго потока
-    int b;
-    bstr>>b;
-
-    if(astr.fail()) {
-        std::cerr << "Недопустимое значение B";
-        return 4;
-    }
-
-
-    if(a>=b){
-        std::cerr <<"Число A больше B ";
-    }
-
-
-    if(a<=0){
-        std::cerr <<"Число A отрицаетльное или ноль";
-    }
-
-
-    if(b<=0){
-        std::cerr <<"Число B отрицательное или ноль";
-    }
-
-
-
-    return 0;
+    catch (std::exception const & e) {
+    std::cerr << e.what() << std::endl;
+    return 1;
+}
 }
